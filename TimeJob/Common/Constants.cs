9f6d3c295a6a56e0c7000c00b0144569f89bf937
Daverick <erick.davimaldo@gmail.com>
2021-10-02 02:37:44 -0500

    unnecessary constants removed

--- TimeJob/Common/Constants.cs
+++ TimeJob/Common/Constants.cs
@@ -6,29 +6,6 @@ namespace TimeJobRecord.Common
 {
   public static class Constants
   {
-    public const string UserDetails = "UserDetails";
-    public const string User = "User";
-    public const string Password = "Password";
-
-    public const string Contacts = "Contacts";
-    public const string Contact = "Contact";
-    public const string EmailDetails = "EmailDetails";
-    public const string Subject = "Subject";
-    public const string FileAttachment = "FileAttachment";
-    public const string BodyText = "BodyText";
-
-    public const string Configuration = "Configuration";
-    public const string ExtraTimeWorked = "ExtraTimeWorked";
-    public const string WorkingHoursPerWeek = "WorkingHoursPerWeek";
-    public const string WorkingDaysPerWeek = "WorkingDaysPerWeek";
-    public const string LunchBreakTime = "LunchBreakTime";
-    public const string AlertTime = "AlertTime";
-    public const string TimeLogFileLocationName = "TimeLogFileLocationName";
-    public const string TimeLogging = "TimeLogging";
-    public const string SoundWarning = "SoundWarning";
-    public const string MinimizeOnStartUp = "MinimizeOnStartUp";
-    public const string EmailCheckBox = "EmailCheckBox";
-
     public const string Version = "1.0";
     public const string Url = "https://github.com/Daveric/JobTimetracker";
     public const string SoundPath = @"SoundFiles\";
